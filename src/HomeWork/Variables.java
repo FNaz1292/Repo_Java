6425c034f8464f9123619b46fcb4c593ff02ff85
FNaz1292 <farahmehran@gmail.com>
2021-11-14 21:14:07 -0500

    Repo_Java

---
+++ src/HomeWork/Variables.java
@@ -0,0 +1,13 @@
+package HomeWork;
+
+public class Variables {
+    public static void main (String[] args){
+        /* store a country's name ina variable.
+        *Pseudo code
+        *
+        *
+         */
+
+    }
+
+}
